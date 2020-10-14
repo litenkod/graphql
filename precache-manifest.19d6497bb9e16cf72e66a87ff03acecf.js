7b1a1de76241b7302a68e00d0428ba0098eb6a12
Magnus Lindeberg <magnus.lindeberg@consid.se>
2020-10-14 08:36:51 +0200

    Updates

---
+++ precache-manifest.19d6497bb9e16cf72e66a87ff03acecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945e9540c488540ca048a94d44c1eff8",
+    "url": "/graphql/index.html"
+  },
+  {
+    "revision": "2435a5d16acb2fe7047d",
+    "url": "/graphql/static/css/main.ffdf8a18.chunk.css"
+  },
+  {
+    "revision": "af817359de73c454ad4e",
+    "url": "/graphql/static/js/2.f2c343cb.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/graphql/static/js/2.f2c343cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2435a5d16acb2fe7047d",
+    "url": "/graphql/static/js/main.4f9af1e0.chunk.js"
+  },
+  {
+    "revision": "4dc11c93f5d733fb9395",
+    "url": "/graphql/static/js/runtime-main.342dabca.js"
+  }
+]);
